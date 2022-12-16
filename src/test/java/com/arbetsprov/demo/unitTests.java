005024106819c30a6329b0b3a2b9805b3fd666e8
moa114 <bermoa@student.chalmers.se>
2022-12-16 16:30:14 +0100

    added priorities and groups

--- src/test/java/com/arbetsprov/demo/unitTests.java
+++ src/test/java/com/arbetsprov/demo/unitTests.java
@@ -1,5 +1,6 @@
 package com.arbetsprov.demo;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,23 +11,32 @@ import java.util.List;
 
 public class unitTests {
 
-    @Test
+    @Test(priority = -3, groups = {"group2"} )
     void testCreateLandscape() {
         LandscapeHandler landscapeHandler = LandscapeHandler.getInstance();
         landscapeHandler.createLandscape("Hittapå", 2000);
-        List<Landscape> landscapeList= landscapeHandler.getLandscapeList();
+        List<Landscape> landscapeList = landscapeHandler.getLandscapeList();
         Assert.assertTrue(landscapeList.contains(landscapeHandler.getLandscape("Hittapå")));
     }
 
-    @Test
-    void testCorrectPopulationData() throws IOException {
+    @Test (priority = -2, groups = {"group2"})
+    void testCorrectPopulationData() throws IOException, JSONException {
         LandscapeHandler landscapeHandler = LandscapeHandler.getInstance();
         landscapeHandler.createLandscape("Hittapå", 2000);
 
-        String populatondata= Files.readString(Path.of("src/test/java/com/arbetsprov/demo/dummydata.txt"));
-        landscapeHandler.enterPopulationStatistic(landscapeHandler.getLandscape("Hittapå"),new JSONArray(populatondata));
-        int pop=landscapeHandler.getLandscape("Hittapå").getPopulation17();
-        Assert.assertEquals(pop,1342388);
+        String populatondata = Files.readString(Path.of("src/test/java/com/arbetsprov/demo/dummydata.txt"));
+        landscapeHandler.enterPopulationStatistic(landscapeHandler.getLandscape("Hittapå"), new JSONArray(populatondata));
+        int pop = landscapeHandler.getLandscape("Hittapå").getPopulation17();
+        Assert.assertEquals(pop, 1342388);
+
+    }
+
+    @Test (priority = -2,groups = {"group2"} )
+    void testGetLandscape() {
+        LandscapeHandler landscapeHandler = LandscapeHandler.getInstance();
+        landscapeHandler.createLandscape("Hittapå", 2000);
+        Landscape madeUp = landscapeHandler.getLandscape("Hittapå");
+        Assert.assertEquals(madeUp.getScbId(),2000);
 
     }
 
